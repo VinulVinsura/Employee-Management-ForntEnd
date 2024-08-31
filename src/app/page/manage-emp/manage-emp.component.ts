4f25725b80acba4feb5935e800ef54990e456fe2
Vinul Vinsura <vinulviksura16@gmail.com>
2024-08-31 14:42:29 +0530

    creat tow-way data binding

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -1,15 +1,30 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
 export class ManageEmpComponent {
-  
+
+
+  public employee:any={
+    firstName:"",
+    lastName:"",
+    email:"",
+    depId:"",
+    roleId:""
+  }
+
+  addEmployee(){
+    console.log(this.employee);
+  }
 
     
 }
